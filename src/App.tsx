51d30252cf9f8f5b086ecd5f777ebc60d0bfa6eb
Yachii19 <johnreinvinuya19@gmail.com>
2025-02-06 13:11:48 +0800

    Fixed some bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,41 @@
-import './App.css'
+import './App.css';
 
-function InformationBox({boxContent, boxColor, boxWidth, boxHeight, borderColor}) {
+interface InformationBoxProps {
+  boxContent: string;
+  boxColor: string;
+  boxWidth: string;
+  boxHeight: string; 
+  borderColor: string;
+}
+
+function InformationBox({ boxContent, boxColor, boxWidth, boxHeight, borderColor }: InformationBoxProps) {
   return (
     <div className='main-container'>
       <div className='name'>John Rein Vinuya</div>
       <div style={{
-        width: boxWidth, 
-        height: boxHeight, 
-        backgroundColor: 
-        boxColor, 
+        width: boxWidth,
+        height: boxHeight,
+        backgroundColor: boxColor,
         border: `2px solid ${borderColor}`,
-
       }} className='box-container'>
         <div className='box-content'>{boxContent}</div>
       </div>
     </div>
-   
-  )
+  );
 }
 
 function App() {
   return (
     <>
-      <InformationBox boxContent={'CPEITEL'} boxColor={'#bbdefb'} boxWidth={200} boxHeight={200} borderColor={'black'}/>
+      <InformationBox 
+        boxContent="CPEITEL" 
+        boxColor="#bbdefb" 
+        boxWidth="200px" 
+        boxHeight="200px" 
+        borderColor="black" 
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
